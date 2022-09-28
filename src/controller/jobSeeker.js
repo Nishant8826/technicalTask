6d5a82ac2b5bb43beb3d794a9ea6e96a10b85aa7
Nishant Rathore <rnishant609@gmail.com>
2022-09-28 10:16:39 +0530

    Final

--- src/controller/jobSeeker.js
+++ src/controller/jobSeeker.js
@@ -5,6 +5,7 @@ const { findOne } = require("../model/jobSeeker.js");
 const createUser = async (req, res) => {
   try {
     const data = req.body;
+    //if skill is not present in body
     if (!data.Skill)
       return res.status(400).send({ status: false, msg: "Skill is mandatory" });
     const saveData = await jobSeekerModel.create(data);
@@ -17,13 +18,16 @@ const createUser = async (req, res) => {
 const login = async (req, res) => {
   try {
     const data = req.body;
+    //searching for email id in database
     const findEmail = await jobSeekerModel.findOne({ Email: data.Email });
     if (!findEmail)
       return res.status(404).send({ status: false, msg: "User not found" });
+    //signing in using jwt
     const jwtToken = await jwt.sign(
       { userId: findEmail._id.toString() },
       "Technical-Round-Connection LLP"
     );
+    //setting header in response body
     res.setHeader("x-api-key", jwtToken);
     res.status(200).send({ status: true, msg: "Success", Token: jwtToken });
   } catch (error) {}
@@ -33,25 +37,29 @@ const fetchSeeker = async (req, res) => {
   try {
     let data = req.query;
     const userId = req.params.userId;
+    //checking for authorisation by comparing userId present in token with Id present in params
     if (res.token.userId != userId)
       return res.status(401).send({ status: false, msg: "Unauthorized" });
-      const findUser = await jobSeekerModel.findById(userId);
-      if (!findUser)
+    //checking email is present in database
+    const findUser = await jobSeekerModel.findById(userId);
+    if (!findUser)
       return res.status(404).send({ status: false, msg: "User Not Found" });
-      if (!data.Skill) {
-        const findData = await jobSeekerModel
-          .find()
-          .sort({ Name: 1 })
-          .collation({ locale: "en" });
+    //when thw user is not filtering with any tag
+    if (!data.Skill) {
+      const findData = await jobSeekerModel
+        .find()
+        .sort({ Name: 1 })
+        .collation({ locale: "en" });
 
-        if (findData.length == 0)
-          return res.status(404).send({ status: false, msg: "No Data Found" });
-        return res.status(200).send({
-          status: true,
-          msg: `${findData.length} Match found`,
-          data: findData,
-        });
-      }
+      if (findData.length == 0)
+        return res.status(404).send({ status: false, msg: "No Data Found" });
+      return res.status(200).send({
+        status: true,
+        msg: `${findData.length} Match found`,
+        data: findData,
+      });
+    }
+    //when the user is filtering with any tags
     const findData = await jobSeekerModel
       .find({ Skill: { $regex: data.Skill } })
       .sort({ Name: 1 })
